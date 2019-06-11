60c9c684448df90f6c656a651a2dd48aa0cea0d2
Jianing Liu <jianing.liu@vanderbilt.edu>
2019-06-10 23:42:12 -0500

    Add new sh files

---
+++ install.sh
@@ -0,0 +1,15 @@
+# Homebrew and Homebrew-Cask
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/cask
+brew tap caskroom/versions
+
+# DEV tools
+brew install node
+
+# Apps
+brew cask install google-chrome
+brew cask install alfred
+
+# Languages
+brew cask install java
+brew install python

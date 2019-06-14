bd9c24e0201c7b295d7e7ab318419a091f6b7db7
Jianing Liu <jianing.liu@vanderbilt.edu>
2019-06-14 12:52:58 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,15 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew tap caskroom/cask
 brew tap caskroom/versions
 
+# Z-Shell
+brew install zsh zsh-completions
+chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Languages
+brew cask install java
+brew install python
+
 # DEV tools
 brew install node
 
@@ -10,6 +19,3 @@ brew install node
 brew cask install google-chrome
 brew cask install alfred
 
-# Languages
-brew cask install java
-brew install python
